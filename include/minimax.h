f13bf2341b793e4eb913a011e655a4dcdc131966
Hugo Viana <hugo@viana.fr>
2025-11-12 15:56:15 +0100

    structure coup

--- include/minimax.h
+++ include/minimax.h
@@ -12,7 +12,10 @@
 typedef struct {
     Jeu etat;
     int evaluation;
-    Noeud* enfant
+    int profondeur;
+    int nombreEnfants;
+    Noeud** enfant;
+    Coup coup;
 } Noeud;
 
 Coup minimax(Jeu* jeu, int profondeur, int (*evaluation)(Jeu*));
